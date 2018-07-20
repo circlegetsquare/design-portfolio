11a32f947084c8adddfb993d19b98ee819aa0627
Joshua Miller <jlmiller07@gmail.com>
2018-07-19 22:44:37 -0500

    adding more layout items and animations

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 window.onload = function(){
-    TweenLite.to("#JM-Logo", 10, {left:600});
+    TweenMax.staggerFrom(["#logo-div", "#name-title", "#title-title", "#nav-menu"], 1, {ease: Power3.easeOut, top:35, opacity:0, delay:.5}, .3);
+    TweenMax.from("#top-content", 1.25, {ease: Power3.easeOut, opacity:0, delay:2.75});
    }
+
+var scrollTarget = document.getElementById("#test-scroll");
+
+scrollTarget.onclick = function() {
+    TweenMax.to(window, 2, {scrollTo:"#section2"});
+  }
