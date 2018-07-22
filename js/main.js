98ec6831f051b793df598fc5a28436e8fff2dacb
Joshua Miller <jlmiller07@gmail.com>
2018-07-22 15:38:57 -0500

    animations, magic scroll, and sticky header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
+// Inital page load animations and 
 window.onload = function(){
-    TweenMax.staggerFrom(["#logo-div", "#name-title", "#title-title", "#nav-menu"], 1, {ease: Power3.easeOut, top:35, opacity:0, delay:.5}, .3);
-    TweenMax.from("#top-content", 1.25, {ease: Power3.easeOut, opacity:0, delay:2.75});
-   }
+    TweenMax.from("#logo-div1", 2, {ease: Power3.easeOut, opacity:0, delay:.5});
+    TweenMax.staggerFrom(["#logo-div2", "#logo-div3", "#logo-div4"], 1, {ease: Power3.easeOut, top:-15,opacity:0, delay:2.5}, .175);
+    TweenMax.to("#logo-div1", 1, {ease: Back.easeInOut.config(1), x:-58, delay:6});
+    TweenMax.staggerFrom(["#name-title", "#title-title"], 1, {ease: Power3.easeOut, top:25, opacity:0, delay:4}, .3);
+    TweenMax.staggerFrom(["#nav-1", "#nav-2", "#nav-3", ], 1, {ease: Power3.easeOut, bottom:15, opacity:0, delay:4.75}, .2);
+    TweenMax.from(["#top-content", "#top-scroller"], 1.25, {ease: Power3.easeOut, opacity:0, delay:4.75});
+   
+    var scrollTarget = document.getElementById("top-scroller");
 
-var scrollTarget = document.getElementById("#test-scroll");
+    scrollTarget.onclick = function() {
+        TweenMax.to(window, 1.25, {scrollTo:{y:"#section2", offsetY:160}, ease:Power2.easeInOut});
+    }
+}
 
-scrollTarget.onclick = function() {
-    TweenMax.to(window, 2, {scrollTo:"#section2"});
-  }
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("top-nav").className = "is-scrolled";
+    } else {
+        document.getElementById("top-nav").className = "unscrolled";
+    }
+}
+
+var controller = new ScrollMagic.Controller();
+
+var scene =new ScrollMagic.Scene({duration: 100, offset:40})
+    .setTween("#top-scroller", {opacity: 0})
+    .addTo(controller);
+
+    /*new ScrollMagic.Scene({
+        duration: 600,    // the scene should last for a scroll distance of 100px
+        offset: 50        // start this scene after scrolling for 50px
+    })
+    .setPin("#top-scroller") // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller*/
